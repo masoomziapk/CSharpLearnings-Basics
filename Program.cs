1799a07976f8747ac3eb77d819b78d5b47a62003
Masoom Zia <masoomzia50@gmail.com>
2025-08-08 19:40:32 +0500

    basic syntax

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Basic Syntax
+
+using System;
+namespace CSharpLearningsBasics
+{
+       class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); // There is difference between Console.WriteLine and Console.Write
+        }
+    }
+}
+
+// Console.WriteLine is used to print the output with a new line at the end, while Console.Write prints the output without a new line.
+
+// Console.Write("Hello, World!"); // This will print "Hello, World!" without a new line at the end.
